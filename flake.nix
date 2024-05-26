1dafb334c1c69d2163aaf4dda7cdb09678bff6a9
Thomas Boerger <thomas@webhippie.de>
2024-05-26 21:36:02 +0200

    ci: apply new workflow structure

--- flake.nix
+++ flake.nix
@@ -3,28 +3,55 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+      url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
-    utils = {
-      url = "github:numtide/flake-utils";
+    devenv = {
+      url = "github:cachix/devenv";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
     };
   };
 
-  outputs = { self, nixpkgs, utils, ... }@inputs:
-    {
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
 
-    } // utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs-18_x
-            nodePackages.npm
-          ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        imports = [
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        ];
+
+        devenv = {
+          shells = {
+            default = {
+              languages = {
+                javascript = {
+                  enable = true;
+                  package = pkgs.nodejs_20;
+                };
+              };
+
+              packages = with pkgs; [];
+            };
+          };
         };
-      }
-    );
+      };
+    };
 }
